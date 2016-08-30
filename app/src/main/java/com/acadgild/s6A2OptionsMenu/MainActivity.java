fccc94dde82205ddd5ec7d44de0a79a561c8d383
admiringsunny <admiringsunny@gmail.com>
2016-08-30 18:22:17 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/s6A2OptionsMenu/MainActivity.java
@@ -0,0 +1,30 @@
+package com.acadgild.s6A2OptionsMenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected " + item.getTitle().toString(), Toast.LENGTH_SHORT ).show();
+        return true;
+    }
+}
